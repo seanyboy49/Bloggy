f8efa3a439328ae85da70070ccf66e634fee33f4
Sean Lee <seanboramlee@gmail.com>
2017-01-27 18:07:04 -0800

    add React-Router and browserHistory to index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+import { Router, browserHistory } from 'react-router'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} />
   </Provider>
   , document.querySelector('.container'));
