9ddeb8a349eb178071a9f3b2c6c6a7e25dc6eaac
Sean Lee <seanboramlee@gmail.com>
2017-01-27 18:11:56 -0800

    add routes componente

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import { Router, browserHistory } from 'react-router'
 import reducers from './reducers';
+import routes from './routes'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
