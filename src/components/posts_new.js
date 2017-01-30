5f6181f53bd4710b4bcfb7b36dee80c722db8f43
Sean Lee <seanboramlee@gmail.com>
2017-01-30 13:09:14 -0800

    add form groups and onSubmit function to posts_new component form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,18 +4,21 @@ import { reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    console.log(title);
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create a New Post</h3>
-        <div classname="form-group">
-          <label>Tittle</label>
-          <input type="text" className="form-control" />
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title}  />
         </div>
-        <div classname="form-group">
+        <div className="form-group">
           <label>Categories</label>
           <input type="text" className="form-control" />
         </div>
-        <div classname="form-group">
+        <div className="form-group">
           <label>Content</label>
           <textarea className="form-control" />
         </div>
