d18664f3d23f321fc41fd75d559020b3e2d30851
Sean Lee <seanboramlee@gmail.com>
2017-01-30 13:57:04 -0800

    add has-danger className as ternary expression to form groups

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,21 +10,22 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
-        <div className="form-group">
+
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}  />
           <div className="text-help">
             {title.touched? title.error : ''}
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
           <div className="text-help">
             {categories.touched? categories.error : ''}
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content} />
           <div className="text-help">
