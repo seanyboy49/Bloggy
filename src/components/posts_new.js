76c458b53502f653ec4f8b37ce47f046e7588e31
Sean Lee <seanboramlee@gmail.com>
2017-01-30 14:15:45 -0800

    add dynamic routing onSubmit back to index

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router'
 
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(()=> {
+        // blog poast has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push with the new
+        // path to navigate to
+        this.context.router.push('/');
+      })
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
 
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
