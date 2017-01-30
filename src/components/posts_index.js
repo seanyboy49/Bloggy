4163fa4a0ec6390ad95ebccbdc510be6074ff748
Sean Lee <seanboramlee@gmail.com>
2017-01-30 14:19:29 -0800

    add renderposts() inside posts_index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -10,6 +10,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
@@ -18,12 +29,17 @@ class PostsIndex extends Component {
             Add a post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+function mapStateToProps(state){
+  return { posts: state.posts.all };
+}
 
-
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
