a72f8c58a0f5870c09be4fb0aa9a85c1dbbd2064
Sean Lee <seanboramlee@gmail.com>
2017-01-30 14:45:50 -0800

    add posts_show component

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import PostsIndex from './components/posts_index'
-import PostsNew from './components/posts_new'
-
-
-const Greeting = () => {
-  return <div>SUP DUDE</div>
-}
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
-
+    <Route path="posts/:id" component={PostsShow}/>
   </Route>
 )
