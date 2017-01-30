941fcb70607ccbc80de2137f7af89735fbbb2990
Sean Lee <seanboramlee@gmail.com>
2017-01-30 13:32:40 -0800

    add mapDispatchToProps to create_posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form'
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 
 class PostsNew extends Component {
@@ -8,7 +9,7 @@ class PostsNew extends Component {
     console.log(handleSubmit);
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -28,7 +29,10 @@ class PostsNew extends Component {
   }
 }
 
+// connect: 1st arg is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
   form: 'PostsNew',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
